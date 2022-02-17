74bb2a1bef49347755dfdf82f84156b0c312fa1b
hmg11123 <you@example.com>
2022-02-17 18:11:05 +0900

    modify

--- front/pages/product/detail/[id].jsx
+++ front/pages/product/detail/[id].jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ClientLayout from "../../../components/ClientLayout";
-import { RsWrapper, WholeWrapper } from "../../../components/commonComponents";
+import {
+  RsWrapper,
+  WholeWrapper,
+  Text,
+} from "../../../components/commonComponents";
 
 const DetailProduct = () => {
   return (
