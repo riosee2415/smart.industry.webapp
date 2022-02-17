83cbdf3de7d1114d6c849db9f1e89295b3c2b41a
hmg11123 <you@example.com>
2022-02-17 18:10:19 +0900

    modify

---
+++ front/pages/product/detail/[id].jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ClientLayout from "../../../components/ClientLayout";
+import { RsWrapper, WholeWrapper } from "../../../components/commonComponents";
+
+const DetailProduct = () => {
+  return (
+    <ClientLayout>
+      <WholeWrapper>
+        <RsWrapper>
+          <Text></Text>
+        </RsWrapper>
+      </WholeWrapper>
+    </ClientLayout>
+  );
+};
+
+export default DetailProduct;
---
+++ front/pages/product/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ClientLayout from "../../components/ClientLayout";
+import { RsWrapper, WholeWrapper } from "../../components/commonComponents";
+
+const ProductList = () => {
+  return (
+    <ClientLayout>
+      <WholeWrapper>
+        <RsWrapper>
+          <Text></Text>
+        </RsWrapper>
+      </WholeWrapper>
+    </ClientLayout>
+  );
+};
+
+export default ProductList;
