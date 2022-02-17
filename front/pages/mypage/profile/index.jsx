645984d0862d13c677af727f9fac3755f060917d
TESTID <4leaf.bhj@gmail.com>
2022-02-17 17:26:50 +0900

    profile index layout

--- front/pages/mypage/profile/index.jsx
+++ front/pages/mypage/profile/index.jsx
@@ -11,11 +11,32 @@ import {
   RsWrapper,
   WholeWrapper,
   Wrapper,
+  Text,
+  TextInput,
+  CommonButton,
 } from "../../../components/commonComponents";
 import { useCallback } from "react";
 import useWidth from "../../../hooks/useWidth";
 import Theme from "../../../components/Theme";
 import { useRouter } from "next/dist/client/router";
+import styled from "styled-components";
+
+const CustomInput = styled(TextInput)`
+  width: 406px;
+  height: 50px;
+  border: 1px solid ${Theme.grey2_C};
+  background-color: ${Theme.lightGrey2_C};
+
+  &:focus {
+    outline: none;
+    border: 1px solid ${Theme.lightGrey2_C};
+    background-color: ${Theme.grey2_C};
+  }
+
+  @media (max-width: 700px) {
+    width: 350px;
+  }
+`;
 
 const Profile = () => {
   ////// GLOBAL STATE //////
@@ -80,7 +101,7 @@ const Profile = () => {
 
       <ClientLayout>
         <WholeWrapper>
-          <RsWrapper margin={`250px 0 0`}>
+          <RsWrapper margin={`250px 0 500px 0`}>
             <Wrapper margin={`40px 0 25px`} al={`flex-start`}>
               HOME | 마이페이지 | 회원정보
             </Wrapper>
@@ -93,6 +114,84 @@ const Profile = () => {
               margin={`0 0 40px`}
             >
               회원정보
+            </Wrapper>
+            <Wrapper width={width < 700 ? `370px` : `600px`}>
+              <Text
+                fontSize={`18px`}
+                fontWeight={`400`}
+                margin={`80px 0 10px 0`}
+                color={Theme.darkGrey_C}
+              >
+                비밀번호 재확인
+              </Text>
+              <Wrapper
+                width={`85%`}
+                dr={width < 700 ? `column` : `row`}
+                margin={`15px 0 55px 0`}
+              >
+                <Text
+                  fontSize={width < 700 ? `14px` : `16px`}
+                  color={Theme.grey_C}
+                >
+                  회원님의 정보를 안전하게 보호하기 위해&nbsp;
+                </Text>
+                <Text
+                  fontSize={width < 700 ? `14px` : `16px`}
+                  color={Theme.grey_C}
+                >
+                  비밀번호를 다시 한번 확인해주세요
+                </Text>
+              </Wrapper>
+              <Wrapper
+                width={`100%`}
+                height={`130px`}
+                dr={`column`}
+                al={`space-between`}
+              >
+                <Wrapper
+                  width={`100%`}
+                  dr={`row`}
+                  ju={`space-between`}
+                  margin={`0 0 30px 0`}
+                >
+                  <Text
+                    fontSize={`18px`}
+                    fontWeight={`700`}
+                    color={`1px solid ${Theme.darkGrey_C}`}
+                    margin={`0 5%`}
+                    display={width < 700 ? `none` : `flex`}
+                  >
+                    아이디
+                  </Text>
+                  <CustomInput
+                    margin={`0 5%`}
+                    placeholder={width < 700 ? `아이디` : ``}
+                  />
+                </Wrapper>
+                <Wrapper width={`100%`} dr={`row`} ju={`space-between`}>
+                  <Text
+                    fontSize={`18px`}
+                    fontWeight={`700`}
+                    color={`1px solid ${Theme.darkGrey_C}`}
+                    margin={`0 5%`}
+                    display={width < 700 ? `none` : `flex`}
+                  >
+                    비밀번호
+                  </Text>
+                  <CustomInput
+                    margin={`0 5%`}
+                    placeholder={width < 700 ? `비밀번호` : ``}
+                  />
+                </Wrapper>
+              </Wrapper>
+              <CommonButton
+                width={`150px`}
+                height={`50px`}
+                radius={`0`}
+                margin={`55px 0 0 15px`}
+              >
+                확인
+              </CommonButton>
             </Wrapper>
           </RsWrapper>
         </WholeWrapper>
