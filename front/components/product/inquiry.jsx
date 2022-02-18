56e29aa38f7cceb0c59ecef1197d90f952830599
hmg11123 <you@example.com>
2022-02-18 17:43:41 +0900

    modify

--- front/components/product/inquiry.jsx
+++ front/components/product/inquiry.jsx
@@ -1,6 +1,23 @@
 import { Table } from "antd";
 import React from "react";
 import { Wrapper } from "../commonComponents";
+import styled from "styled-components";
+import Theme from "../Theme";
+
+const CustomTable = styled(Table)`
+  & .ant-table-thead tr {
+    border-top: 1px solid ${Theme.grey2_C} !important;
+    border-bottom: 1px solid ${Theme.grey2_C} !important;
+  }
+
+  & .ant-table-thead .ant-table-cell {
+    height: 40px;
+    text-align: center;
+  }
+  & .ant-table-thead .ant-table-cell::before {
+    display: none;
+  }
+`;
 
 const Inquiry = () => {
   ////// DATAIVEW //////
@@ -9,27 +26,32 @@ const Inquiry = () => {
     {
       title: "번호",
       dataIndex: "id",
+      width: `10%`,
     },
     {
       title: "제목",
-      dataIndex: "id",
+      dataIndex: "title",
+      width: `60%`,
     },
     {
       title: "작성자",
-      dataIndex: "id",
+      dataIndex: "author",
+      width: `10%`,
     },
     {
       title: "작성일",
-      dataIndex: "id",
+      dataIndex: "createdAt",
+      width: `10%`,
     },
     {
       title: "조회수",
-      dataIndex: "id",
+      dataIndex: "hit",
+      width: `10%`,
     },
   ];
   return (
     <Wrapper>
-      <Table style={{ width: `100%` }} size="middle" columns={columns} />
+      <CustomTable style={{ width: `100%` }} size="middle" columns={columns} />
     </Wrapper>
   );
 };
