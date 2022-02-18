a5725440ea1d986dfe96d7d6aff9306f7b57b6d8
hmg11123 <you@example.com>
2022-02-18 12:07:45 +0900

    modify

--- front/pages/product/[id]/index.jsx
+++ front/pages/product/[id]/index.jsx
@@ -12,7 +12,14 @@ const DetailProduct = () => {
     <ClientLayout>
       <WholeWrapper>
         <RsWrapper>
-          <Wrapper margin={`280px 0 0`}></Wrapper>
+          <Wrapper margin={`280px 0 0`}>
+            <Wrapper dr={`row`} ju={`space-between`}>
+              <Wrapper width={`calc(50% - 30px)`} padding={`30px 110px 40px`}>
+                test
+              </Wrapper>
+              <Wrapper width={`calc(50% - 30px)`}>test</Wrapper>
+            </Wrapper>
+          </Wrapper>
         </RsWrapper>
       </WholeWrapper>
     </ClientLayout>
