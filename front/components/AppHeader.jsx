124bdea177a9028867419b060cd2b5189d7455db
hmg11123 <you@example.com>
2022-02-28 21:43:01 +0900

    modify

--- front/components/AppHeader.jsx
+++ front/components/AppHeader.jsx
@@ -775,22 +775,58 @@ const AppHeader = () => {
           ju={`flex-start`}
           padding={`0 10px`}
         >
-          <Wrapper minWidth={`80px`} margin={`0 5px`}>
+          <Wrapper
+            minWidth={`80px`}
+            margin={`0 5px`}
+            cursor={`pointer`}
+            onClick={() =>
+              moveLinkHandler(
+                `/product?menu=${
+                  headerCategoryList && headerCategoryList[0].MenuId
+                }&category=${headerCategoryList && headerCategoryList[0].id}`
+              )
+            }
+          >
             건설기계
           </Wrapper>
-          <Wrapper minWidth={`80px`} margin={`0 5px`}>
+          <Wrapper
+            minWidth={`80px`}
+            margin={`0 5px`}
+            onClick={() => moveLinkHandler(`/product?isUsed=true`)}
+            cursor={`pointer`}
+          >
             중고장비
           </Wrapper>
-          <Wrapper minWidth={`80px`} margin={`0 5px`}>
+          <Wrapper
+            minWidth={`80px`}
+            margin={`0 5px`}
+            onClick={() => moveLinkHandler(`/product?isSale=true`)}
+            cursor={`pointer`}
+          >
             특가상품
           </Wrapper>
-          <Wrapper minWidth={`80px`} margin={`0 5px`}>
+          <Wrapper
+            minWidth={`80px`}
+            margin={`0 5px`}
+            onClick={() => moveLinkHandler(`/lease?type=임대문의`)}
+            cursor={`pointer`}
+          >
             임대문의
           </Wrapper>
-          <Wrapper minWidth={`80px`} margin={`0 5px`}>
+          <Wrapper
+            minWidth={`80px`}
+            margin={`0 5px`}
+            onClick={() => moveLinkHandler(`/lease?type=사업자문의`)}
+            cursor={`pointer`}
+          >
             사업자문의
           </Wrapper>
-          <Wrapper minWidth={`80px`} margin={`0 5px`}>
+          <Wrapper
+            minWidth={`80px`}
+            margin={`0 5px`}
+            onClick={() => moveLinkHandler(`/lease?type=장비판매의뢰`)}
+            cursor={`pointer`}
+          >
             장비판매의뢰
           </Wrapper>
         </Wrapper>
